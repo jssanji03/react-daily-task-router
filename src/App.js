4aa1025783738ae2dc2c82508bbd62f7b8965d20
Jess <jssanji03@gmail.com>
2022-08-21 20:31:57 +0800

    navigate use

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import {
   HashRouter,
-  NavLink,Routes,Route
+  NavLink,Routes,Route,useNavigate
 } from 'react-router-dom';
 
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return (
+    <>
+      <p>這是 Todo 頁面 </p>
+      <Logout/>
+    </>
+  );
 };
 const Login = () => {
-  return <p>這是登入頁面</p>;
+  return <p>請登入！</p>;
+};
+const Logout = () => {
+  let navigate = useNavigate();
+  return <button className='btn' onClick={()=>navigate('/login')}>登出</button>;
 };
 const Register = () => {
   return <p>這是註冊頁面</p>;
@@ -24,26 +32,27 @@ function App() {
       <HashRouter>
         <div className="nav-link">
           <NavLink to="/">
-            <p>回到首頁</p>
+            <p>首頁</p>
           </NavLink>
           <NavLink to="/register">
             <p>註冊頁面</p>
           </NavLink> 
-          <NavLink to="/login">
-            <p>登入頁面</p>
-          </NavLink>
           <NavLink to="/todo">
             <p>Todo 頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
           </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         {/* 練習區 */}
         
         <Routes>
-          <Route path="/" /> 
+          <Route index element={<Home />} /> 
           <Route path="register" element={<Register />} /> 
-          <Route path="login" element={<Login />} /> 
           <Route path="todo" element={<Todo />} /> 
+          <Route path="login" element={<Login />} /> 
+          <Route path="logout" element={<Logout />} /> 
         </Routes>
         
       </HashRouter>
